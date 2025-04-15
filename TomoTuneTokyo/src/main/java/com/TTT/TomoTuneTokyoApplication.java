e83f89c11befd86d7816d8c1c176e8b38a3f3ee1
LYD1ATA <fpahsgkdla98@gmail.com>
2025-04-15 11:57:50 +0900

    Project Test

---
+++ TomoTuneTokyo/src/main/java/com/TTT/TomoTuneTokyoApplication.java
@@ -0,0 +1,13 @@
+package com.TTT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomoTuneTokyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomoTuneTokyoApplication.class, args);
+	}
+
+}
