36eef62e8acf2d2f5a266aa89a9c201323b30590
LYD1ATA <fpahsgkdla98@gmail.com>
2025-04-23 10:51:52 +0900

    Live Server가아닌실행단계에서 작동하게 변경

--- TomoTuneTokyo/src/main/java/com/TTT/configure/SecurityConfig.java
+++ TomoTuneTokyo/src/main/java/com/TTT/configure/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.TTT.configure;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -13,26 +15,25 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        // return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); // 테스트를 위한 코드
     }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/css/**", "/JS/**", "/images/**", "/js/**", "/user/**").permitAll() // ✅ 이 줄 중요!
-                .anyRequest().authenticated()
-            )
-            .formLogin(login -> login
-                .loginPage("/user/login")    // 여기에 해당하는 컨트롤러 + HTML이 실제 있어야 함!
-                .defaultSuccessUrl("/")
-                .permitAll()
-            )
-            .logout(logout -> logout
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                .logoutSuccessUrl("/")
-                .permitAll()
-            );
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/css/**", "/JS/**", "/images/**", "/js/**", "/user/**").permitAll() // ✅ 이 줄 중요!
+                        .anyRequest().authenticated())
+                .formLogin(login -> login
+                        // .loginPage("/user/login") // 여기에 해당하는 컨트롤러 + HTML이 실제 있어야 함! (테스트를 위해 일단 주석)
+                        .defaultSuccessUrl("/")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                        .logoutSuccessUrl("/")
+                        .permitAll());
 
         return http.build();
     }
