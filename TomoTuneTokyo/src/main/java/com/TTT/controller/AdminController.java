23da47bf6e998c0a85c5c4505f5b5d11353ddd31
LYD1ATA <fpahsgkdla98@gmail.com>
2025-04-28 16:21:49 +0900

    admin list added

--- TomoTuneTokyo/src/main/java/com/TTT/controller/AdminController.java
+++ TomoTuneTokyo/src/main/java/com/TTT/controller/AdminController.java
@@ -3,6 +3,12 @@ package com.TTT.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping("/admin")
@@ -13,4 +19,14 @@ public class AdminController {
         return "admin/adminIndex";
     }
 
+    @GetMapping("/list")
+    public String userList(HttpServletRequest request, @RequestParam(name = "page", required = false) String page) {
+        if ("true".equals(request.getHeader("HX-Request"))) {
+            page = (page == null) ? "1" : "3";
+            System.out.println(page);
+            return "admin/forLiveServer";
+        } else {
+            return "redirect:/admin/main";
+        }
+    }
 }
