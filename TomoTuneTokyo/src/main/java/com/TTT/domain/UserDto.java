84b5cd7e16d2d3b52ac9176e30f6546bfbcafdd2
LG gram <LG gram@DESKTOP-0D4TLUO>
2025-05-15 16:27:19 +0900

    밴드 결성 insert완성

--- TomoTuneTokyo/src/main/java/com/TTT/domain/UserDto.java
+++ TomoTuneTokyo/src/main/java/com/TTT/domain/UserDto.java
@@ -6,8 +6,7 @@ import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
-@Data
-@Component
+
 public class UserDto {
 	//users
 	private String user_id;
@@ -22,5 +21,75 @@ public class UserDto {
 	private String user_memo;
 	//user_profile
 	private String nickname;
+	
+	
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
+	}
+	public LocalDateTime getDeleted_at() {
+		return deleted_at;
+	}
+	public void setDeleted_at(LocalDateTime deleted_at) {
+		this.deleted_at = deleted_at;
+	}
+	public String getUser_memo() {
+		return user_memo;
+	}
+	public void setUser_memo(String user_memo) {
+		this.user_memo = user_memo;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	
+	
 
 }
