c746b29657f7bde2ee3950fee34f812face3a280
dDeadBunnyb <fpahsgkdla5421@gmail.com>
2025-05-18 19:50:48 +0900

    글 작성기능 데이터 파싱까지

--- TomoTuneTokyo/src/main/java/com/TTT/controller/publicController.java
+++ TomoTuneTokyo/src/main/java/com/TTT/controller/publicController.java
@@ -1,16 +1,23 @@
 package com.TTT.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.TTT.domain.UserDto;
 import com.TTT.service.AdminService;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequestMapping({ "/user", "/admin" })
@@ -20,25 +27,26 @@ public class publicController {
     AdminService adminService;
 
     @GetMapping("/write")
-    public Object writePost(HttpServletRequest request, Model model) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && !(accept.contains("application/json"))) {
-            String uri = request.getRequestURI();
-            if (uri.startsWith("/admin")) {
-                if ("true".equals(request.getHeader("HX-Request"))) {
-                    String prevUri = request.getHeader("referer")
-                            .substring(request.getHeader("referer").lastIndexOf("/") + 1);
-                    model.addAttribute("prev", prevUri);
-                    return "public/write";
-                } else {
-                    return "redirect:/admin/main";
-                }
+    public String writePost(HttpServletRequest request, Model model) {
+        String uri = request.getRequestURI();
+        if (uri.startsWith("/admin")) {
+            if ("true".equals(request.getHeader("HX-Request"))) {
+                String prevUri = request.getHeader("referer")
+                        .substring(request.getHeader("referer").lastIndexOf("/") + 1);
+                model.addAttribute("prev", prevUri);
+                return "public/write";
+            } else {
+                return "redirect:/admin/main";
             }
-            return "public/write";
-        } else {
-
-            return "";
         }
+        return "public/write";
+    }
+
+    @PostMapping("/doWrite")
+    public ResponseEntity<?> doPost(@RequestParam("content") String content,
+            @RequestParam(name = "images", required = false) List<MultipartFile> images) {
+        System.out.println(images);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/test")
