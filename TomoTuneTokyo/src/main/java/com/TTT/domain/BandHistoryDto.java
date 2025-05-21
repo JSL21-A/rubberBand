7976e7137a3c457c97f2b8ce2c4283294ecd9136
Sion <Sion@LAPTOP-IBSV2OD9>
2025-05-21 14:12:51 +0900

    마이페이지 - 이력서 작성 기능 완성

---
+++ TomoTuneTokyo/src/main/java/com/TTT/domain/BandHistoryDto.java
@@ -0,0 +1,13 @@
+package com.TTT.domain;
+
+import lombok.Data;
+
+@Data
+public class BandHistoryDto {
+
+	private int resumeId; 
+    private String bandName;
+    private String bandPart;
+    private String bandStart; 
+    private String bandEnd;   
+}
